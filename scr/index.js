a860135aaca9988d5b31884ca0147492eedde8e9
MLaura963 <marialaurasantanab@gmail.com>
2025-08-29 22:07:15 -0300

    Adiciona README.md com descrição do projeto

--- scr/index.js
+++ scr/index.js
@@ -76,31 +76,88 @@ async function playRaceEngine(character1,character2) {
       totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
       totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
       
-      await logRollResult(character1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);
-      await logRollResult(character2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
+      await logRollResult(
+        character1.NOME, 
+        "manobrabilidade", 
+        diceResult1, 
+        character1.MANOBRABILIDADE);
+
+      await logRollResult(
+        character2.NOME, 
+        "manobrabilidade", 
+        diceResult2, 
+        character2.MANOBRABILIDADE);
     };
 
     if (block == "CONFRONTO") {
       let powerResul1 = diceResult1 + character1.PODER;
       let powerResul2 = diceResult2 + character2.PODER;
-      console.log();
+
+      console.log(`${character1.NOME} confrotou com ${character2.NOME}! 🥊`);
+
+      await logRollResult(
+        character1.NOME,
+        "poder", 
+        diceResult1, 
+        character1.PODER);
+
+      await logRollResult(
+        character2.NOME, 
+        "poder", 
+        diceResult2, 
+        character2.PODER)
+      
+
+      if(powerResul1 > powerResul2 && character2.PONTOS > 0) {
+        console.log(`${character1.NOME} venceu o confronto e ganhou um ponto! 
+        ${character2.NOME} perdeu 1 ponto`)
+        character1.PONTOS++;
+        character2.PONTOS--;
+      }
+
+      if(powerResul2 > powerResul1 && character1.PONTOS > 0) {
+        console.log(`${character2.NOME} venceu o confronto e ganhou um ponto! 
+        ${character1.NOME} perdeu 1 ponto`)
+        character2.PONTOS++;
+        character1.PONTOS--;
+      }
+
+      if(powerResul1 === powerResul2){
+        console.log("Confronto empatado!")
+        };
     };
-    
+
+
+    //verifica vencedor
     if(totalTestSkill1> totalTestSkill2){
-        console.log(`${character1.NOME} venceu a rodada!`)
+      console.log(`${character1.NOME} venceu a rodada!`)
+      character1.PONTOS++;
     } else if(totalTestSkill2> totalTestSkill1){
       console.log(`${character2.NOME} venceu a rodada!`)
+      character2.PONTOS++;
     }
 
-    console.log("-----------------------------------------")
+    console.log("-------------------------------------------")
     };
+}
 
+async function declareWinner(character1, character2) {
+  console.log("Resultado final:");
+  console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`);
+  console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`);
+
+  if (character1.PONTOS > character2.PONTOS){
+    console.log(`\n${character1.NOME} venceu a corrida! Parabéns! 🏆`);
+  }else if (character2.PONTOS > character1.PONTOS){
+    console.log(`\n${character2.NOME} venceu a corrida! Parabéns! 🏆`);
+  }else console.log("A corrida terminou em empate");
 }
 
 //ROUNDS
 (async function main() { 
     console.log("🏁🚨Começou a corrida!")
 
-  await playRaceEngine(player1,player2)
+  await playRaceEngine(player1,player2);
+  await declareWinner(player1, player2);
 })();
 
